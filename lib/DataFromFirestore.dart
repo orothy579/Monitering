d81f193df86b996a237c35a5f1cbe64d69fa9e3f
orothy579 <ftsky2050@naver.com>
2023-10-10 17:57:19 +0900

    Graph

--- lib/DataFromFirestore.dart
+++ lib/DataFromFirestore.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'graph.dart';
+
 
 class CO2moniter extends StatelessWidget {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +14,11 @@ class CO2moniter extends StatelessWidget {
       body: Column(
         children: [
           StreamBuilder(
-            stream: _firestore.collection('my_collection').orderBy('timestamp', descending: true).limit(1).snapshots(),
+            stream: _firestore
+                .collection('my_collection')
+                .orderBy('timestamp', descending: true)
+                .limit(1)
+                .snapshots(),
             builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
               if (snapshot.hasError) {
                 return Center(child: Text('Something went wrong'));
@@ -24,16 +29,21 @@ class CO2moniter extends StatelessWidget {
               }
 
               // Assuming 'xbee_data' contains the data we want to display
-              String xbeeData = snapshot.data!.docs.first['xbee_data'] ?? "Data Unavailable";
+              String xbeeData =
+                  snapshot.data!.docs.first['xbee_data'] ?? "Data Unavailable";
 
               return Center(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Text('CO2 PPM', style: Theme.of(context).textTheme.headline4),
+                    Text('CO2 PPM',
+                        style: Theme.of(context).textTheme.headline4),
                     SizedBox(height: 20),
-                    Text(xbeeData, style: Theme.of(context).textTheme.headline3),
-                    SizedBox(height: 20,),
+                    Text(xbeeData,
+                        style: Theme.of(context).textTheme.headline3),
+                    SizedBox(
+                      height: 20,
+                    ),
                     ElevatedButton(
                       onPressed: () {
                         Navigator.push(
@@ -52,5 +62,5 @@ class CO2moniter extends StatelessWidget {
       ),
     );
   }
-
 }
+
--- lib/graph.dart
+++ lib/graph.dart
@@ -8,12 +8,13 @@ class GraphPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<SalesData> chartData = [
-      SalesData(2010, 35),
-      SalesData(2011, 28),
-      SalesData(2012, 34),
-      SalesData(2013, 32),
-      SalesData(2014, 40)
+      SalesData(DateTime(2010), 35.0),
+      SalesData(DateTime(2011), 28.0),
+      SalesData(DateTime(2012), 11.0),
+      SalesData(DateTime(2013), 22.0),
+      SalesData(DateTime(2014), 30.0)
     ];
+
 
     return Scaffold(
         body: Center(
@@ -29,10 +30,12 @@ class GraphPage extends StatelessWidget {
                     ]))));
   }
 
-  class SalesData {
+
+
+}
+
+class SalesData {
   SalesData(this.year, this.sales);
   final DateTime year;
   final double sales;
-  }
-
 }
